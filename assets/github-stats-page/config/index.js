ba0d4ed16f74b98a01d5edaba945a0a9421a1c7f
Ken Fukuyama <kenfdev@gmail.com>
2018-06-10 21:29:23 +0900

    Stop using custom template
    Signed-off-by: Ken Fukuyama <kenfdev@gmail.com>

--- assets/github-stats-page/config/index.js
+++ assets/github-stats-page/config/index.js
@@ -45,12 +45,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../../../github-stats-page/dist/index.html'),
+    index: path.resolve(__dirname, '../../../github-stats-page/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../../../github-stats-page/dist'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '?path=/',
+    assetsPublicPath: 'https://rawgit.com/kenfdev/faas-github-stats/master/github-stats-page/dist/',
 
     /**
      * Source Maps
