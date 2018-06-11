23668070e617323343d777235081c61fe0b5ebe3
Ken Fukuyama <kenfdev@gmail.com>
2018-06-11 23:08:51 +0900

    Fixed some minor issues and added index to table.
    Signed-off-by: Ken Fukuyama <kenfdev@gmail.com>

--- assets/github-stats-page/src/components/Stats.vue
+++ assets/github-stats-page/src/components/Stats.vue
@@ -1,51 +1,70 @@
 <template>
   <div class="stats">
-    <v-layout v-if="stats.total" row wrap>
-      <v-flex xs12 sm12 md4>
-        <v-card>
-          <v-card-title>
-            <div>
-              <h3 class="headline mb-0">Total Stars: {{ stats.total.stars }}</h3>
-            </div>
-            <PieChart :chart-data="stars" :options="options" :width="200" style="margin: auto;"></PieChart>
-          </v-card-title>
-        </v-card>
-      </v-flex>
-      <v-flex xs12 sm12 md4>
-        <v-card>
-          <v-card-title>
-            <h3 class="headline mb-0">Total Commits: {{ stats.total.commits }}</h3>
-            <PieChart :chart-data="commits" :options="options" :width="200" style="margin: auto;"></PieChart>
-          </v-card-title>
-        </v-card>
-      </v-flex>
-      <v-flex xs12 sm12 md4>
-        <v-card>
-          <v-card-title>
-            <h3 class="headline mb-0">Total Forks: {{ stats.total.forks }}</h3>
-            <PieChart :chart-data="forks" :options="options" :width="200" style="margin: auto;"></PieChart>
-          </v-card-title>
-        </v-card>
-      </v-flex>
-      <v-flex xs12>
-        <v-toolbar>
-          <v-toolbar-title>Contributors: {{ stats.uniqueAuthors }}</v-toolbar-title>
-        </v-toolbar>
-      </v-flex>
-      <v-flex xs12>
-        <v-data-table :headers="headers" :items="contribs" class="elevation-1" :pagination.sync="pagination">
-          <template slot="items" slot-scope="props">
-            <td>
-              <v-avatar>
-                <img :src="'https://github.com/' + props.item.key + '.png'">
-              </v-avatar>
-            </td>
-            <td>{{ props.item.key }}</td>
-            <td class="text-xs-right">{{ props.item.value }}</td>
-          </template>
-        </v-data-table>
-      </v-flex>
-    </v-layout>
+    <div v-if="!loading">
+      <v-layout v-if="stats.total" row wrap>
+        <v-flex xs12 sm12 md4>
+          <v-card>
+            <v-card-title>
+              <v-flex xs12>
+                <h3 class="headline mb-0">Stars: {{ stats.total.stars }}</h3>
+              </v-flex>
+              <PieChart :chart-data="stars" :options="options" :width="200" style="margin: auto;"></PieChart>
+            </v-card-title>
+          </v-card>
+        </v-flex>
+        <v-flex xs12 sm12 md4>
+          <v-card>
+            <v-card-title>
+              <v-flex xs12>
+                <h3 class="headline mb-0">Commits: {{ stats.total.commits }}</h3>
+              </v-flex>
+              <PieChart :chart-data="commits" :options="options" :width="200" style="margin: auto;"></PieChart>
+            </v-card-title>
+          </v-card>
+        </v-flex>
+        <v-flex xs12 sm12 md4>
+          <v-card>
+            <v-card-title>
+              <v-flex xs12>
+                <h3 class="headline mb-0">Forks: {{ stats.total.forks }}</h3>
+              </v-flex>
+              <PieChart :chart-data="forks" :options="options" :width="200" style="margin: auto;"></PieChart>
+            </v-card-title>
+          </v-card>
+        </v-flex>
+        <v-flex xs12>
+          <v-toolbar>
+            <v-toolbar-title>Contributors: {{ stats.uniqueAuthors }}</v-toolbar-title>
+          </v-toolbar>
+        </v-flex>
+        <v-flex xs12>
+          <v-data-table :headers="headers" :items="contribs" class="elevation-1" :pagination.sync="pagination">
+            <template slot="items" slot-scope="props">
+              <td>{{ props.item.index + 1 }}</td>
+              <td>
+                <v-avatar>
+                  <img :src="'https://github.com/' + props.item.key + '.png'">
+                </v-avatar>
+              </td>
+              <td>{{ props.item.key }}</td>
+              <td class="text-xs-right">{{ props.item.value }}</td>
+            </template>
+          </v-data-table>
+        </v-flex>
+      </v-layout>
+      <v-layout v-if="!stats.total">
+        <v-container>
+          <v-flex xs12>
+            <p class="grey--text">
+              No stats fetched? Try loading it again.
+            </p>
+          </v-flex>
+          <v-flex xs12>
+            <v-btn color="pink" @click="fetch">Load</v-btn>
+          </v-flex>
+        </v-container>
+      </v-layout>
+    </div>
     <v-layout v-else row wrap align-center>
       <v-container>
         <v-flex xs12>
@@ -102,10 +121,12 @@ export default {
   },
   data() {
     return {
+      loading: true,
       headers: [
-        { text: 'Avatar', value: 'avatar' },
-        { text: 'Name', value: 'key' },
-        { text: 'Commits', value: 'value' }
+        { text: 'No', value: 'no', align: 'center', sortable: false },
+        { text: 'Avatar', value: 'avatar', align: 'center', sortable: false },
+        { text: 'Name', value: 'key', align: 'center' },
+        { text: 'Commits', value: 'value', align: 'center' }
       ],
       pagination: {
         sortBy: 'value',
@@ -121,46 +142,66 @@ export default {
           display: false
         },
         responsive: true
-        // maintainAspectRatio: false
       },
       contribs: []
     };
   },
-  created() {
-    axios
-      .post('/github-stats', {
-        org: 'openfaas'
-      })
-      .then(res => {
-        const json = res.data;
-        this.stats = json;
-        const ary = Object.keys(json.byRepo).map(k => {
-          return { key: k, value: json.byRepo[k] };
-        });
-
-        const sortedByStars = ary.sort((a, b) => {
-          return b.value.stars - a.value.stars;
-        });
-        this.stars = generateChartData(sortedByStars, 'stars');
-
-        const sortedByCommits = ary.sort((a, b) => {
-          return b.value.commits - a.value.commits;
-        });
-        this.commits = generateChartData(sortedByCommits, 'commits');
-
-        const sortedByForks = ary.sort((a, b) => {
-          return b.value.forks - a.value.forks;
-        });
-        this.forks = generateChartData(sortedByForks, 'forks');
-
-        this.contribs = Object.keys(json.byLogin)
-          .map(k => {
-            return { key: k, value: json.byLogin[k] };
-          })
-          .sort((a, b) => {
-            return b.value - a.value;
-          });
+  methods: {
+    fetch() {
+      this.loading = true;
+      this.loadStats().finally(() => {
+        this.loading = false;
       });
+    },
+    loadStats() {
+      return axios
+        .post('/github-stats', {
+          org: 'openfaas'
+        })
+        .then(res => {
+          const json = res.data;
+          this.stats = json;
+          const ary = Object.keys(json.byRepo).map(k => {
+            return { key: k, value: json.byRepo[k] };
+          });
+
+          const sortedByStars = ary.sort((a, b) => {
+            return b.value.stars - a.value.stars;
+          });
+          this.stars = generateChartData(sortedByStars, 'stars');
+
+          const sortedByCommits = ary.sort((a, b) => {
+            return b.value.commits - a.value.commits;
+          });
+          this.commits = generateChartData(sortedByCommits, 'commits');
+
+          const sortedByForks = ary.sort((a, b) => {
+            return b.value.forks - a.value.forks;
+          });
+          this.forks = generateChartData(sortedByForks, 'forks');
+
+          this.contribs = Object.keys(json.byLogin)
+            .map(k => {
+              return { key: k, value: json.byLogin[k] };
+            })
+            .sort((a, b) => {
+              return b.value - a.value;
+            })
+            .map((v, index) => {
+              return {
+                key: v.key,
+                value: v.value,
+                index
+              };
+            });
+        });
+    }
+  },
+  created() {
+    this.loading = true;
+    this.loadStats().finally(() => {
+      this.loading = false;
+    });
   }
 };
 </script>
