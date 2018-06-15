48f80816e5fccb0e85df617e7be845551115a50c
Ken Fukuyama <kenfdev@gmail.com>
2018-06-15 16:59:04 +0900

    Support pagination to collect all data
    Signed-off-by: Ken Fukuyama <kenfdev@gmail.com>

--- github-stats/lib/github-helper.js
+++ github-stats/lib/github-helper.js
@@ -1,33 +1,57 @@
-const {
-    fetchContributors,
-    createReport
-} = require('./utils');
+const { fetchContributors, createReport } = require('./utils');
 
-const GitHubApi = require("github");
+const octokit = require('@octokit/rest');
 
 class GithubHelper {
-    constructor(org, token) {
-        this.org = org;
-        this.github = new GitHubApi();
-        this.github.authenticate({
-            type: 'token',
-            token: token,
-        })
+  constructor(org, token) {
+    this.org = org;
+    this.github = octokit();
+    this.github.authenticate({
+      type: 'token',
+      token: token
+    });
+  }
+
+  async paginate(method, options) {
+    let response = await method(options);
+    let { data } = response;
+    while (this.github.hasNextPage(response)) {
+      if (/^202/.test(response.meta.status)) {
+        throw new Error(
+          'The stats are currently being calculated. Please request again later.'
+        );
+      }
+      if (!/^204/.test(response.meta.status)) {
+        response = await this.github.getNextPage(response);
+        data = data.concat(response.data);
+      }
     }
 
-    createReport() {
-        return this.github.repos.getForOrg({
-            org: this.org,
-            page: 1,
-            per_page: 100
-        }).then(res => {
-            const promises = fetchContributors(res.data, (repo) => this.github.repos.getStatsContributors({
-                owner: this.org,
-                repo: repo.name
-            }));
-            return Promise.all(promises)
-        }).then(createReport);
+    if (!Array.isArray(data)) {
+      data = [];
     }
+    return data;
+  }
+
+  async createReport() {
+    const data = await this.paginate(this.github.repos.getForOrg, {
+      org: this.org,
+      page: 1,
+      per_page: 100
+    });
+
+    const promises = fetchContributors(data, async repo => {
+      const data = await this.paginate(this.github.repos.getStatsContributors, {
+        owner: this.org,
+        repo: repo.name
+      });
+      return data;
+    });
+
+    const repoInfos = await Promise.all(promises);
+
+    return createReport(repoInfos);
+  }
 }
 
 module.exports = GithubHelper;
--- github-stats/lib/utils.js
+++ github-stats/lib/utils.js
@@ -1,77 +1,64 @@
 function fetchContributors(repos, fetchFactory) {
-    return repos.map(repo => {
-        return fetchFactory(repo).then(res => {
-            const status = res.meta.status;
-
-            let contributors = [];
-            if (/^202/.test(status)) {
-                throw new Error("The stats are currently being calculated. Please request again later.");
-            }
-            if (!/^204/.test(status)) {
-                // empty repos return 204 (no content) so need to checked
-                contributors = res.data.map(d => {
-                    return {
-                        login: d.author.login,
-                        total: d.total,
-                    };
-                })
-            }
-            return {
-                repo: repo.name,
-                stars: repo.stargazers_count,
-                forks: repo.forks_count,
-                contributors,
-            };
-        });
-    })
+  return repos.map(repo => {
+    return fetchFactory(repo).then(data => {
+      const contributors = data.map(d => {
+        return {
+          login: d.author.login,
+          total: d.total
+        };
+      });
+      return {
+        repo: repo.name,
+        stars: repo.stargazers_count,
+        forks: repo.forks_count,
+        contributors
+      };
+    });
+  });
 }
 
 function createReport(repoInfos) {
-    let report = {
-        total: {
-            commits: 0,
-            forks: 0,
-            stars: 0,
-        },
-        uniqueAuthors: 0,
-        byLogin: {},
-        byRepo: {},
-    };
+  let report = {
+    total: {
+      commits: 0,
+      forks: 0,
+      stars: 0
+    },
+    uniqueAuthors: 0,
+    byLogin: {},
+    byRepo: {}
+  };
 
-    repoInfos.forEach(s => {
+  repoInfos.forEach(s => {
+    const repo = report.byRepo[s.repo];
+    if (!repo) {
+      report.byRepo[s.repo] = {
+        commits: 0,
+        stars: s.stars,
+        forks: s.forks
+      };
+    }
 
-        const repo = report.byRepo[s.repo];
-        if (!repo) {
-            report.byRepo[s.repo] = {
-                commits: 0,
-                stars: s.stars,
-                forks: s.forks,
-            }
-        }
+    report.total.stars += s.stars;
+    report.total.forks += s.forks;
 
-        report.total.stars += s.stars;
-        report.total.forks += s.forks;
-
-        s.contributors.forEach(c => {
-
-            const loginCount = report.byLogin[c.login];
-            if (!loginCount) {
-                report.byLogin[c.login] = 0;
-            }
-            report.byLogin[c.login] += c.total;
-            report.byRepo[s.repo].commits += c.total;
-            report.total.commits += c.total;
-
-        });
-
+    s.contributors.forEach(c => {
+      const loginCount = report.byLogin[c.login];
+      if (!loginCount) {
+        report.byLogin[c.login] = 0;
+      }
+      report.byLogin[c.login] += c.total;
+      report.byRepo[s.repo].commits += c.total;
+      report.total.commits += c.total;
     });
+  });
 
-    report.uniqueAuthors = Object.keys(report.byLogin).length;
+  report.uniqueAuthors = Object.keys(report.byLogin).length;
 
-    return report;
+  return report;
 }
 
 module.exports = {
-    fetchContributors,
-    createReport,
-}
+  fetchContributors,
+  createReport
+};
