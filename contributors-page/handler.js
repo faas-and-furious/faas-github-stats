ce3644a0fa82134bb26c6ad295e42341b38f27de
Ken Fukuyama <kenfdev@gmail.com>
2018-06-17 22:05:42 +0900

    Delete contributors-page
    Signed-off-by: Ken Fukuyama <kenfdev@gmail.com>

--- contributors-page/handler.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const fs = require('fs');
-
-const html = fs.readFileSync(`${__dirname}/contributors.html`).toString();
-
-module.exports = (context, callback) => {
-  callback(undefined, html);
-};
